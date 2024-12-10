dd2256658f750695c1757145c08187708f1e1672
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-12-10 23:12:47 +0500

    fix: update alt text for image and adjust button margin in NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,18 +9,18 @@ export default function NotFound() {
     <>
     <Breadcrumb title="404 Not Found" subtitle="404 Not Found" />
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center justify-center w-[913px] h-[644px]">
+      <div className="flex flex-col items-center justify-center w-[913px]">
         <Image
           className="mt-[-200px]"
           src={'/404.png'}
-          alt="not found"
+          alt="Not Found"
           width={580}
           height={600}
         />
         <h1 className="text-[#152970] font-bold mt-4">
           Oops! The page you requested was not found!
         </h1>
-        <Button className='mt-10 px-7'><Link href={"/"}>Back To Home</Link></Button>
+        <Button className='px-7 mt-8'><Link href={"/"}>Back To Home</Link></Button>
       </div>
     </div>
     </>
