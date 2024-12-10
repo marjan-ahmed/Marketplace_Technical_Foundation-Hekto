af9008a754b85af4485f677b693e73297495bcc3
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-12-10 23:28:01 +0500

    fix: update font style in ProductCart2 component for consistency

--- src/app/components/ProductCart2.tsx
+++ src/app/components/ProductCart2.tsx
@@ -20,13 +20,13 @@ function ProductCart2({ bg, src, alt, width, height, productName }: ProductCart2
               height={height}/>
             </div>
             <div className="flex flex-col gap-2 justify-center items-center mt-2">
-                <h1 className="text-[17.3px] text-[#151875] font-bold">{productName}</h1>
+                <h1 className="text-[17.3px] text-[#151875] font-bold font-josefin">{productName}</h1>
                 <div className="flex gap-[5px] w-[42px] h-[10px]">
                     <div className="w-[10px] h-[10px] rounded-full bg-[#DE9034]"></div>
                     <div className="w-[10px] h-[10px] rounded-full bg-[#EC42A2]"></div>
                     <div className="w-[10px] h-[10px] rounded-full bg-[#8568FF]"></div>
                 </div>
-                <div className="w-[96px] text-[14px] h-[14px] flex gap-3">
+                <div className="w-[96px] text-[14px] h-[14px] flex gap-3 font-josefin">
                   <div className="w-[43px] h-[14px] text-[#151875]">$26.00</div>
                   <div className="w-[43px] h-[14px] line-through text-[#FB2E86] ">$42.00</div>
                 </div>
