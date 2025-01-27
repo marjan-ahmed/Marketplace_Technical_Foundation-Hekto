c99cfa3dbc1656b2d530280b02369fd552561e51
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-01-27 22:53:20 +0500

    add api dir

---
+++ src/app/api/shipengine/get-rates/route.ts
@@ -0,0 +1,70 @@
+import { shipengine } from "../../../lib/helper/shipEngine"; 
+import { Address, Package } from "../../../../../type"; 
+import { NextRequest } from "next/server";
+
+const POST = async(req: NextRequest) => {
+  try {
+    const {
+      shipeToAddress,
+      packages,
+    }: { shipeToAddress: Address; packages: Package[] } = await req.json();
+
+    
+    if (!shipeToAddress || !packages) {
+      return new Response(
+        JSON.stringify({
+          error: "Missing required fields: shipeToAddress and packages",
+        }),
+        { status: 400 }
+      );
+    }
+
+
+    const shipFromAddress: Address = {
+      name: "Michael Smith",
+      phone: "+1 555 987 6543",
+      addressLine1: "456 Oak Avenue",
+      addressLine2: "Suite 200",
+      cityLocality: "Los Angeles",
+      stateProvince: "CA",
+      postalCode: "90001",
+      countryCode: "US",
+      addressResidentialIndicator: "no", 
+    };
+
+    
+    const shipmentDetails = await shipengine.getRatesWithShipmentDetails({
+      shipment: {
+        shipTo: shipeToAddress,
+        shipFrom: shipFromAddress,
+        packages: packages,
+      },
+      rateOptions: {
+        carrierIds: [
+          "se-224931",
+          "se-224932",
+          "se-224933",
+          "se-228363",
+        ].filter(Boolean), 
+      },
+    });
+
+    
+    console.log("Ship To Address:", shipeToAddress);
+    console.log("Packages:", packages);
+    console.log("Shipment Details:", shipmentDetails);
+
+    
+    return new Response(
+      JSON.stringify({ shipeToAddress, packages, shipmentDetails }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error fetching shipping rates:", error)
+    return new Response(JSON.stringify({ error: error }), {
+      status: 500,
+    });
+  }
+}
+
+export { POST };
